175c062aea57edad4cf210e0862bf953a939936c
_Arom <117594980+Ranksine@users.noreply.github.com>
2024-04-05 15:55:48 -0600

    1st update

---
+++ main.py
@@ -0,0 +1,16 @@
+def mi_Funcion(nombresini, apellido):
+    print('Saludos desde mi función')
+    print(f'Nombre: {nombresini}, Apellido: {apellido}')
+
+
+mi_Funcion('Juanillo', 'Perezillo')
+mi_Funcion('Karla', 'Larita')
+mi_Funcion('Karla', 'lopez')
+def mi_Funcion(nombresini, apellido):
+    print('Saludos desde mi función')
+    print(f'Nombre: {nombresini}, Apellido: {apellido}')
+
+
+mi_Funcion('Juanillo', 'Perezillo')
+mi_Funcion('Karla', 'Larita')
+mi_Funcion('Karla', 'lopez')
